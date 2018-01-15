d108676d08a550604daeade2e781e99ce631094e
James <jshand27@gmail.com>
2018-01-15 23:19:29 +0000

    added single review page

---
+++ wp-content/themes/side/single-review.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Template Name: Single Review
+ *
+ * @package WordPress
+ * @subpackage Side
+ * @since Side 1.0
+ */
+?>
+
+<?php get_header(); ?>
+<div class="container main">
+
+  <div class="left-col">
+    <div class="col">
+      <div class="logo-col">
+        <h1>sideAsideB</h2>
+      </div>
+      <div class="menu-col">
+        <ul class="menu">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+        </ul>
+        <ul class="social">
+          <li><a href="#">facebook</a></li>
+          <li><a href="#">twitter</a></li>
+          <li><a href="#">instagram</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div class="half center br">
+    <div class="half-col">
+      <h1 class="hollow">Side A</h1>
+      <h3 class="author">James Shand</h3>
+      <p>We have been writing lots of different reviews that you might be interested in reading, they are from a wide variety of artists.</p>
+      <p>We have been writing lots of different reviews that you might be interested in reading, they are from a wide variety of artists. We have been writing lots of different reviews that you might be interested in reading, they are from a wide variety of artists..</p>
+      <p class="p-cols">
+        Some of the stuff we've actually been writing about has not been super interesting. Other stuff has been super interesting and you will maybe enjoy some of it.
+      </p>
+    </div>
+  </div>
+  <div class="half center">
+    <div class="half-col">
+      <h1 class="hollow">Side B</h1>
+      <h3 class="author">Paul Smith</h3>
+      <p>We have been writing lots of different reviews that you might be interested in reading, they are from a wide variety of artists.</p>
+      <p>We have been writing lots of different reviews that you might be interested in reading, they are from a wide variety of artists. We have been writing lots of different reviews that you might be interested in reading, they are from a wide variety of artists..</p>
+      <p class="p-cols">
+        Some of the stuff we've actually been writing about has not been super interesting. Other stuff has been super interesting and you will maybe enjoy some of it.
+      </p>
+    </div>
+  </div>
+</div>
