0f519452648e183f08d0b884e0c4e49dbc30b982
James <jshand27@gmail.com>
2018-02-15 00:48:41 +0000

    single artist page now displays all reviews for albums related to artist

--- wp-content/themes/side/single-artist.php
+++ wp-content/themes/side/single-artist.php
@@ -8,14 +8,20 @@
  */
 ?>
 
+<?php 
+
+$name = get_field('name');
+
+?>
+
 <?php get_header(); ?>
 <div class="container main">
 
   <div class="left-col br">
     <div class="col">
       <div class="logo-col">
-        <h1>Mitski</h1>
-        <h1>Puberty 2</h1>
+        <h1>Artist</h1>
+        <h1>Profile</h1>
       </div>
       <div class="menu-col">
         <ul class="menu">
@@ -31,8 +37,63 @@
   </div>
   <div class="full center">
     <div class="artist-image artist-image--large" style="background:url('<?php echo get_field('image'); ?>');"></div>
-    <h1 class="hollow"><?php echo get_field('name'); ?></h1>
+    <h1 class="hollow"><?php echo $name; ?></h1>
     <?php echo get_field('description'); ?>
+    <h2>Reviews</h2>
+    <?php 
+
+      $albums = get_posts([
+        'numberposts' => -1,
+        'post_type' => 'album',
+        'meta_query' => [
+          [
+            'key' => 'artist',
+            'value' => get_the_ID(),
+            'compare' => 'LIKE'
+          ]
+        ]
+      ]);
+
+      $reviews = [];
+
+      foreach($albums as $album){
+
+        $review = get_posts([
+          'numberposts' => -1,
+          'post_type' => 'review',
+          'meta_query' => [
+            [
+              'key' => 'album',
+              'value' =>  $album->ID,
+              'compare' => 'LIKE'
+            ]
+          ]
+        ]);
+
+        array_push($reviews, $review);
+
+      }
+
+
+      foreach($reviews as $review) {
+
+        $reviewID = $review[0]->ID;
+        $url = get_permalink($reviewID);
+        $album = get_field('album', $reviewID);
+        $albumID = $album[0]->ID;
+        $albumName = get_field('name', $albumID);
+        $albumArt = get_field('image', $albumID);
+
+        echo '<div class="review-thumb">
+              <a href="'.$url.'">
+              <img src="'.$albumArt.'" alt="'.$albumName.'"/>
+              </a>
+              </div>';
+  
+      }
+      
+
+    ?>
   </div>
 
 </div>
