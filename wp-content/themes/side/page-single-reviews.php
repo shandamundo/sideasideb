b7bb0b7bb3f1ab505a7471c1ad820ea24bb6ae21
James Shand <james@frogspark.co.uk>
2018-10-16 00:02:00 +0100

    Revert "Revert "Develop""

--- wp-content/themes/side/page-single-reviews.php
+++ wp-content/themes/side/page-single-reviews.php
@@ -13,17 +13,20 @@
   <div class="left-col br">
     <div class="col">
       <div class="logo-col">
-        <h1><a href="/home">sideAsideB</a></h1>
+        <div class="logo-col__info">
       </div>
-      <div class="menu-col">
+      </div>
+      <div class="menu-col menu-col--no-border">
+        <h1 class="review-site-title"><a href="/home">sideAsideB</a></h1>
         <ul class="menu">
           <li><a href="/">Home</a></li>
           <li><a href="/about">About</a></li>
-          <li><a href="/reviews">Reviews</a></li>
+          <li><a href="/singles" class="active">Singles</a></li>
+          <li><a href="/albums">Albums</a></li>
         </ul>
         <ul class="social">
           <li><a href="https://www.facebook.com/sideasidebblog" target="_blank">facebook</a></li>
-          <li><a href="https://www.instagram.com/sideasidebblog/" target="_blank">instagram</a></li>
+          <li><a href="https://instagram.com/sideasidebblog" target="_blank">instagram</a></li>
         </ul>
       </div>
     </div>
@@ -44,10 +47,12 @@
           $id = $review->ID;
           $url = get_permalink($id);
           $artist = get_field('artist', $id);
+          $albumArt = get_field('single_art', $id);
           $name = $artist->post_title;
 
           echo '<li>
                   <a href="'.$url.'">
+                    <img src="'.$albumArt.'" class="review-list__art" />
                     <span class="reviews-list__thumb">
                   <b style="text-decoration: underline;">'.$name.'</b><br/>'.get_field('title', $id).'
                     </span>
